50cc669d93d7da5e53ea18e0cffb0e5459fe22f3
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-16 10:03:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
+monto=st.numbers_intut("Ingrese el monto:",min_value = 0, max_value=10000, value=1000)
+interes.numbers_intut("Ingrese el interes:",min_value = 0.0, max_value=1.0, value=0.10)
+anios.numbers_intut("Ingrese el año:",value=1)
+numero_pagos.numbers_intut("Ingrese el número de pagos:", value=12)
+
 cuota = lf.cuota_prestamo(1000,0.10,5,12)
 st.write(cuota)
 
