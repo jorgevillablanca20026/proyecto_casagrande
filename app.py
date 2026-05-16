b5aa1631f6927d52781f77ed4658d8b68bb315b1
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-16 10:04:25 -0500

    Fix typo in input function for monto

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
-monto=st.numbers_intut("Ingrese el monto:",min_value = 0, max_value=10000, value=1000)
+monto=st.numbers_intut("Ingrese el monto:", min_value = 0, max_value=10000, value=1000)
 interes.numbers_intut("Ingrese el interes:",min_value = 0.0, max_value=1.0, value=0.10)
 anios.numbers_intut("Ingrese el año:",value=1)
 numero_pagos.numbers_intut("Ingrese el número de pagos:", value=12)
