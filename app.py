21bd3caa9cd93cf36ae5abc5e1196a7f536a10bc
jorgevillablanca20026 <jorge.villablanca@casagrande.edu.ec>
2026-05-16 10:05:20 -0500

    Fix input methods and update loan calculation

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ for uploaded_file in uploaded_files:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
-monto=st.numbers_intut("Ingrese el monto:", min_value = 0, max_value=10000, value=1000)
-interes.numbers_intut("Ingrese el interes:",min_value = 0.0, max_value=1.0, value=0.10)
-anios.numbers_intut("Ingrese el año:",value=1)
-numero_pagos.numbers_intut("Ingrese el número de pagos:", value=12)
+monto = st.number_input("Ingrese el monto:", min_value = 0 , max_value = 10000, value=1000)
+interes = st.number_input("Ingrese el interes:",min_value = 0.0 , max_value = 1.0, value=0.10)
+anios = st.number_input("Ingrese el número de años del prestamo:",value=1)
+numero_pagos = st.number_input("Ingrese el número pagos anuales:" , value = 12)
 
-cuota = lf.cuota_prestamo(1000,0.10,5,12)
-st.write(cuota)
+cuota = lf.cuota_prestamo(monto, interes,anios,numero_pagos)
+st.write("Su cuota mensual es: ",cuota)
 
